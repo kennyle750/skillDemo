4996ed6eb53bc27124a65bfc476bded07e3ae191
redagent750 <37556745+redagent750@users.noreply.github.com>
2022-05-01 23:32:17 -0800

    changed test case

--- skillDemoTester.java
+++ skillDemoTester.java
@@ -9,6 +9,12 @@ public class skillDemoTester{
 
     @Test
     public void test1(){
-        assertEquals(2, skillDemo.subtraction(2,2));
+        assertEquals(2, skillDemo.subtraction(4,2));
     }
 }
+ 
+ 
+//javac -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar skillDemoTester.java
+
+//java -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore skillDemoTester
+//
